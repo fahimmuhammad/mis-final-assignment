29e3f170f0e5631fd6da19c3cd34be2a21a03556
fahimmuhammad <fahim1996.df@gmail.com>
2025-01-10 01:58:31 +0600

    some variables taken

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const mealResults = document.getElementById('meal-results');
+const showAllBtn = document.getElementById('show-all-btn');
+
+
